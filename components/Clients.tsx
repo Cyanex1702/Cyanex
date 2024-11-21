d0f4fe6ebcfad7809d41821f83532b8513192f6e
Cyanex <117669695+Cyanex1702@users.noreply.github.com>
2024-11-22 03:16:22 +0500

    Update Clients.tsx

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -16,12 +16,12 @@ const Clients = () => {
       <div className="flex flex-col items-center max-lg:mt-10">
         <div
           // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive design
-          className="bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem]"
+          className="bg-white dark:bg-inherit dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem]"
         >
           <InfiniteMovingCards
             items={testimonials}
             direction="right"
-            speed="slow"
+            speed="normal"
           />
         </div>
 
