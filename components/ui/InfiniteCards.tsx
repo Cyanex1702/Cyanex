2c795f8d5a6ca20a5a59664a7ea075ccec19cb6b
Cyanex <117669695+Cyanex1702@users.noreply.github.com>
2024-11-22 01:47:08 +0500

    Update InfiniteCards.tsx

--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -3,14 +3,15 @@ import { useCallback } from 'react';
 import { cn } from "@/lib/utils";
 import Image from 'next/image';
 import React, { useEffect, useState } from "react";
-import {FaRegArrowAltCircleLeft, FaRegArrowAltCircleRight} from "react-icons/fa";
+import { FaRegArrowAltCircleLeft, FaRegArrowAltCircleRight } from "react-icons/fa";
+
 export const InfiniteMovingCards = ({
-                                      items,
-                                      direction = "left",
-                                      speed = "fast",
-                                      pauseOnHover = true,
-                                      className,
-                                    }: {
+  items,
+  direction = "left",
+  speed = "fast",
+  pauseOnHover = true,
+  className,
+}: {
   items: {
     quote: string;
     name: string;
@@ -30,23 +31,17 @@ export const InfiniteMovingCards = ({
   const [startPosition, setStartPosition] = useState(0);
   const [scrollLeft, setScrollLeft] = useState(0);
 
-
-
-
-
-
-
   const getDirection = useCallback(() => {
     if (containerRef.current) {
       if (direction === "left") {
         containerRef.current.style.setProperty(
-            "--animation-direction",
-            "forwards"
+          "--animation-direction",
+          "forwards"
         );
       } else {
         containerRef.current.style.setProperty(
-            "--animation-direction",
-            "reverse"
+          "--animation-direction",
+          "reverse"
         );
       }
     }
@@ -85,7 +80,6 @@ export const InfiniteMovingCards = ({
     addAnimation();
   }, [addAnimation]);
 
-
   const handleMouseDown = (e: React.MouseEvent) => {
     setIsDragging(true);
     setStartPosition(e.pageX - containerRef.current!.offsetLeft);
@@ -105,7 +99,7 @@ export const InfiniteMovingCards = ({
     if (!isDragging) return;
     e.preventDefault();
     const x = e.pageX - containerRef.current!.offsetLeft;
-    const walk = (x - startPosition) * 3; // 3 is the scroll speed
+    const walk = (x - startPosition) * 3;
     containerRef.current!.scrollLeft = scrollLeft - walk;
   };
 
@@ -118,7 +112,7 @@ export const InfiniteMovingCards = ({
   const handleTouchMove = (e: React.TouchEvent) => {
     if (!isDragging) return;
     const x = e.touches[0].pageX - containerRef.current!.offsetLeft;
-    const walk = (x - startPosition) * 3; // 3 is the scroll speed
+    const walk = (x - startPosition) * 3;
     containerRef.current!.scrollLeft = scrollLeft - walk;
   };
 
@@ -145,86 +139,85 @@ export const InfiniteMovingCards = ({
   };
 
   return (
-      <div className="relative">
-        <div
-            ref={containerRef}
-            className={cn(
-                "scroller relative  z-20 w-screen overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
-                className
-            )}
-            onMouseDown={handleMouseDown}
-            onMouseLeave={handleMouseLeave}
-            onMouseUp={handleMouseUp}
-            onMouseMove={handleMouseMove}
-            onTouchStart={handleTouchStart}
-            onTouchMove={handleTouchMove}
-            onTouchEnd={handleTouchEnd}
+    <div className="relative min-h-[450px] md:min-h-[600px] w-full overflow-hidden">
+      <div
+        ref={containerRef}
+        className={cn(
+          "scroller relative z-20 w-full overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+          className
+        )}
+        onMouseDown={handleMouseDown}
+        onMouseLeave={handleMouseLeave}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
+        <ul
+          ref={scrollerRef}
+          className={cn(
+            "flex min-w-full shrink-0 gap-4 md:gap-16 py-4 w-max flex-nowrap",
+            start && "animate-scroll",
+            pauseOnHover && "hover:[animation-play-state:paused]"
+          )}
         >
-          <ul
-              ref={scrollerRef}
-              className={cn(
-                  "flex min-w-full shrink-0 gap-16 py-4 w-max flex-nowrap",
-                  start && "animate-scroll",
-                  pauseOnHover && "hover:[animation-play-state:paused]"
-              )}
-          >
-            {items.map((item, idx) => (
-                <li
-                    className="w-[90vw] max-w-full relative rounded-2xl border border-b-0
-             flex-shrink-0 border-slate-800 p-5 md:p-16 md:w-[60vw]"
-                    style={{
-                      background: "rgb(4,7,29)",
-                      backgroundColor: "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-                    }}
-                    key={idx}
-                >
-                  <blockquote>
-                    <div
-                        aria-hidden="true"
-                        className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
-                    ></div>
-                    <span className="relative z-20 text-sm md:text-lg leading-[1.6] text-white font-normal">
+          {items.map((item, idx) => (
+            <li
+              className="w-[85vw] md:w-[60vw] h-full relative rounded-2xl border border-b-0 
+                flex-shrink-0 border-slate-800 px-4 py-8 md:p-16"
+              style={{
+                background: "rgb(4,7,29)",
+                backgroundColor: "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+              }}
+              key={idx}
+            >
+              <blockquote className="h-full flex flex-col justify-between">
+                <div
+                  aria-hidden="true"
+                  className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
+                ></div>
+                <span className="relative z-20 text-sm md:text-lg leading-[1.6] text-white font-normal">
                   {item.quote}
                 </span>
-                    <div className="relative z-20 mt-6 flex flex-row items-center">
-                      <div className="me-3">
-                        <Image
-                            src={item.profileImage}
-                            alt={item.name}
-                            className="w-12 h-12 rounded-full object-cover"
-                            width={250}
-                            height={250}
-                        />
-                      </div>
-                      <span className="flex flex-col gap-1">
-                    <span className="text-xl font-bold leading-[1.6] text-white">
+                <div className="relative z-20 mt-6 flex flex-row items-center">
+                  <div className="me-3">
+                    <Image
+                      src={item.profileImage}
+                      alt={item.name}
+                      className="w-10 h-10 md:w-12 md:h-12 rounded-full object-cover"
+                      width={250}
+                      height={250}
+                    />
+                  </div>
+                  <span className="flex flex-col gap-1">
+                    <span className="text-lg md:text-xl font-bold leading-[1.6] text-white">
                       {item.name}
                     </span>
-                    <span className="text-sm leading-[1.6] text-white-200 font-normal">
+                    <span className="text-xs md:text-sm leading-[1.6] text-white-200 font-normal">
                       {item.title}
                     </span>
                   </span>
-                    </div>
-                  </blockquote>
-                </li>
-            ))}
-          </ul>
-        </div>
-        <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 flex gap-4 z-20">
-          <button
-              onClick={scrollLeftFunc}
-
-              className="p-4 bg-gray-800 text-white rounded-full hover:bg-yellow-400 hover:text-black"
-          >
-            <FaRegArrowAltCircleLeft size={18} />
-          </button>
-          <button
-              onClick={scrollRightFunc}
-              className="p-4 bg-gray-800 text-white rounded-full hover:bg-yellow-400 hover:text-black"
-          >
-            <FaRegArrowAltCircleRight size={18}/>
-          </button>
-        </div>
+                </div>
+              </blockquote>
+            </li>
+          ))}
+        </ul>
       </div>
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 flex gap-4 z-20 mb-4">
+        <button
+          onClick={scrollLeftFunc}
+          className="p-3 md:p-4 bg-gray-800 text-white rounded-full hover:bg-yellow-400 hover:text-black"
+        >
+          <FaRegArrowAltCircleLeft size={16} className="md:w-[18px] md:h-[18px]" />
+        </button>
+        <button
+          onClick={scrollRightFunc}
+          className="p-3 md:p-4 bg-gray-800 text-white rounded-full hover:bg-yellow-400 hover:text-black"
+        >
+          <FaRegArrowAltCircleRight size={16} className="md:w-[18px] md:h-[18px]" />
+        </button>
+      </div>
+    </div>
   );
 };
